46ef54d7cccf57a956901eff084606c00a14d777
Adelmo <ad2d2silva@gmailc.om>
2021-06-17 00:41:37 -0300

    Refazendo os passos da Introdução ao Java: Criamos o packages; Criamos Classe Criamos o método main Trabalhamos com funcoes e variaveis.

---
+++ src/main/java/treinando1/Medidas.java
@@ -0,0 +1,55 @@
+// 1 - Pacote
+package treinando1;
+
+// 2 - Referencia as Bibliotecas
+
+
+// 3- Classe
+public class Medidas {
+    // 3.1 - Atributos - Características
+
+    // 3.2 - Métodos ( faz mas nao da retorno) e Funções ( faz e da um retorno, resultado...)
+    public static void main(String[] args){
+        System.out.println("Cálculo de Areas");
+
+        // calculo de area - reduzido
+
+        int largura = 4;
+        int comprimento = 3;
+
+        System.out.println("Para a larguea de " + largura + "m² e o comprimento de " + comprimento + "m² a area é de "
+                + largura * comprimento + "m²");
+    }
+
+    public void calcularModoExtenso(){
+
+        // Calculo de área - Exemplo : o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5;  // largura recebe 4
+        comprimento = 6;
+
+        resultado = largura * comprimento;
+        System.out.println("Para a larguea de " + largura + "m² e o comprimento de " + comprimento + "m² a area é de "
+                + resultado + "m²");
+
+
+    }
+
+    public void calcularAreaModoCompacto(){
+
+        // calculo de area - reduzido
+
+        int largura = 4;
+        int comprimento = 3;
+
+        System.out.println("Para a larguea de " + largura + "m² e o comprimento de " + comprimento + "m² a area é de "
+                + largura * comprimento + "m²");
+
+
+
+    }
+
+}
