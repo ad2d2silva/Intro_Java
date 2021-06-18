a66964e5e6721a25e92d3f3908b6642f30ba99f3
Adelmo <ad2d2silva@gmailc.om>
2021-06-17 22:40:49 -0300

    Criamos novos metodos de contar ate dez, contagem regressiva. Aprendemos a usar a Classe Scanner para ler a escolha do Usuario no Console.

--- src/main/java/treinando1/Medidas.java
+++ src/main/java/treinando1/Medidas.java
@@ -4,18 +4,88 @@ package treinando1;
 // 2 - Referencia as Bibliotecas
 
 
+import java.util.Scanner;
+
 // 3- Classe
 public class Medidas {
     // 3.1 - Atributos - Características
 
     // 3.2 - Métodos ( faz mas nao da retorno) e Funções ( faz e da um retorno, resultado...)
     public static void main(String[] args){
+        // Condicional = Verificar uma condição - Fazer uma pergunta para uma pessoa, hardware ou software (API)
 
-        calculoModoCurto();
-        calcularModoExtenso();
+        // utilizar a classe Scanner do JAVA para ler a escolha do usuário no console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("M E N U  D E   O P Ç Õ E S ");
+        System.out.println("C - Calcular Area Modo Curto ");
+        System.out.println("D - Contar ate Dez ");
+        System.out.println("E - Calcular Area Modo Extenso ");
+        System.out.println("I - If Simples ");
+        System.out.println("R - Contagem regressiva de dez a zero ");
+        System.out.println("Digite a opção desejada: ");
+
+        String opcao = scanner.next();
+
+        // switch = selecionar o comportamento do programa conforme a escolha da pessoa ou do software
+
+        //String opcao = "curto";
+
+        switch (opcao){
+
+            case "C":
+            case "c":
+                System.out.println("Você escolheu o método calcularAreaModoCurto");
+                calcularAreaModoCurto();
+                break;
+            case "D":
+            case "d":
+                System.out.println("Você escolheu a opção Contar a ate Dez");
+                contarAteDez();
+                break;
+            case "E":
+            case "e":
+                System.out.println("Você escolheu o método calcularAreaModoExtenso");
+                calcularAreaModoExtenso();
+            case "I":
+            case "i":
+                System.out.println("Você escolheu o método ifSimples");
+                ifsimples();
+                break;
+            case "R":
+            case "r":
+                System.out.println("Você escolheu o método Contagem Regressiva");
+                contagemRegressiva();
+                break;
+            default:
+                System.out.println("Você escolheu outro valor que não esta associado ");
+                break;
+
+
+
+        }
+
 
     }
-    public static void calculoModoCurto(){
+    public static void ifsimples(){
+        // Condicional = Verificar uma condição - Fazer uma pergunta para uma pessoa, hardware ou software (API)
+
+        // if = se
+        // else = senão
+        //Exemplo: criar uma variavel tipo String :
+        String modo = "curto";
+
+        if (modo == "curto"){
+            calcularAreaModoCurto();
+        }
+        else {
+            calcularAreaModoExtenso();
+        }
+
+
+    }
+
+
+    public static void calcularAreaModoCurto(){
         System.out.println("Cálculo de Areas Modo Curto");
 
         // calculo de area - reduzido
@@ -27,7 +97,7 @@ public class Medidas {
                 + largura * comprimento + "m²");
 
     }
-    public static void calcularModoExtenso(){
+    public static void calcularAreaModoExtenso(){
 
         System.out.println("Cálculo de Areas Modo Extenso");
         // Calculo de área - Exemplo : o tamanho do tapete ou do piso
@@ -44,6 +114,27 @@ public class Medidas {
 
 
     }
+        public static void contarAteDez(){
+
+        // Loops ou Repetições
+        // for = repetição incondicional
+            System.out.println("Contagem Crescente");
+
+        for (int numero = 1; numero <=10; numero++){
+            System.out.println(numero);
+
+        }
+
+        }
+        public static void contagemRegressiva(){
+            System.out.println("Contagem Regressiva");
+
+            for (int numero = 10; numero > -1; numero--){
+                System.out.println(numero);
+            }
+
+        }
+
 
 
 
